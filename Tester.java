e70beae2f2c672c673037ee0f7a9e75c39b4c283
Sofi0208 <soficrisafulli@hotmail.com.ar>
2020-09-17 11:08:15 -0300

    Conflicto 3

--- Tester.java
+++ Tester.java
@@ -1,6 +1,6 @@
 public class prueba{ 
 	public static void main(String[]){ 
-		Hora h=new Hora(02,30)
+		Hora h=new Hora(02,30); 
 		System.out.println("La cantidad de horas es: "+h.obtenerHora());
 		System.out.println("La cantidad de minutos es: "+h.obtenerMinutos()); 
 	}
